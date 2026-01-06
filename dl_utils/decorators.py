02d18d5c88855aeb7fedb2e4abb46f126d388a02
coopa33 <dan.yu.h97@gmail.com>
2026-01-06 18:30:27 +0100

    removed debug trace, added comments

--- dl_utils/decorators.py
+++ dl_utils/decorators.py
@@ -4,17 +4,20 @@ import pdb
 
 def timer(func):
     """Print the runtime of the decorated function"""
-    pdb.set_trace()
+    # define the timer to wrap around original function. @functools.wraps ensures metadate of original function is maintained
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
         start_time = time.perf_counter()
+
+        # exectute original function with parameters and return value
         value = func(*args, **kwargs)
+
         end_time = time.perf_counter()
         run_time = end_time - start_time
 
         print(f"Finished {func.__name__!r} in {run_time:.4f} sec")
-
         return value
+    
     return wrapper_timer
     
 
@@ -22,13 +25,7 @@ def timer(func):
     
 
 if __name__=="__main__":
-        
-    @timer
-    def example():
-        print("hello world!")
-
-
-    example()
+    pass
 
     
 
